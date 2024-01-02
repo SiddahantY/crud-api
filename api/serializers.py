9cd56195ecc74e8053b1c3c2c81dd5fa24c6e577
SiddahantY <siddhantyadav750@gmail.com>
2024-01-02 22:26:06 +0530

    final commit

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Note
 
 class NoteSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model=Note
         fields=('id','title','text')
 
